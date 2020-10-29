036143afea4d0c6ecda025cbe64a488ff2fa6801
Hoboxs <huboux.noah@gmail.com>
2020-10-29 17:04:33 -0400

    Set the configuration for the project.

---
+++ rockstars-web/src/main/java/Comp3095RockstarsApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3095RockstarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp3095RockstarsApplication.class, args);
+    }
+
+}
