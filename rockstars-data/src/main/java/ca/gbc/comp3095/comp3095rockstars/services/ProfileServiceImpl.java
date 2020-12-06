cef87247e10bd351ebdc3f81893bd3ef500a4aaf
alexbalez <alexbalez@gmail.com>
2020-12-06 09:14:07 -0500

    more failed tests

--- rockstars-data/src/main/java/ca/gbc/comp3095/comp3095rockstars/services/ProfileServiceImpl.java
+++ rockstars-data/src/main/java/ca/gbc/comp3095/comp3095rockstars/services/ProfileServiceImpl.java
@@ -2,11 +2,19 @@ package ca.gbc.comp3095.comp3095rockstars.services;
 
 import ca.gbc.comp3095.comp3095rockstars.model.Profile;
 import ca.gbc.comp3095.comp3095rockstars.model.User;
+import ca.gbc.comp3095.comp3095rockstars.repository.ProfileRepository;
+import ca.gbc.comp3095.comp3095rockstars.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class ProfileServiceImpl implements ProfileService{
+
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
     @Override
     public Set<Profile> findAll() {
 
